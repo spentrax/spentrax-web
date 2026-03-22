885509f70660276806f17ff3a30c2e13c837a129
Augustine <augustin@digivalsolutions.com>
2026-03-22 16:15:56 +0530

    Refactor Navbar component to use React Router's Link for navigation

--- src/components/Landing/Navbar.tsx
+++ src/components/Landing/Navbar.tsx
@@ -1,17 +1,19 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
-    return (
-      <nav className="nav">
-        <div className="logo">⚡ Spentrax</div>
-  
-        <div className="links">
-          <a href="#">Features</a>
-          <a href="#">Pricing</a>
-          <a href="#">Docs</a>
-          <a href="/login">Login</a>
-          <button className="btn-primary">Get Started</button>
-        </div>
-      </nav>
-    );
-  };
-  
-  export default Navbar;
+  return (
+    <nav className="nav">
+      <div className="logo">⚡ Spentrax</div>
+
+      <div className="links">
+        <Link to="/features">Features</Link>
+        <Link to="/pricing">Pricing</Link>
+        <Link to="/docs">Docs</Link>
+        <Link to="/login">Login</Link>
+        <button className="btn-primary">Get Started</button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
